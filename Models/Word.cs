6274038f6013318c1ce7487a71f4a2a78e08b447
João Victor <jvictorsousa2015@gmail.com>
2024-08-26 16:15:00 -0300

    V1_JogoForca

---
+++ Models/Word.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJogoForca.Models
+{
+    public class Word
+    {
+        public Word(String tips, string text)
+        {
+            this.Tips = tips;
+            this.Text = text;
+        }
+        public string Tips { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Repositories/WordRepositorie.cs
@@ -0,0 +1,34 @@
+﻿using AppJogoForca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJogoForca.Repositories
+{
+    public class WordRepositorie
+    {
+        private List<Word> _words;
+
+        public WordRepositorie()
+        {
+            _words = new List<Word>();
+            _words.Add(new Word("Nome", "Joao".ToUpper()));
+            _words.Add(new Word("Fruta", "Maça".ToUpper()));
+            _words.Add(new Word("Objeto", "Cadeira".ToUpper()));
+            _words.Add(new Word("Comida", "Baiana".ToUpper()));
+            _words.Add(new Word("Carro", "Mercedes".ToUpper()));
+            _words.Add(new Word("Vegetal", "Cenoura".ToUpper()));
+            _words.Add(new Word("País", "Alemanha".ToUpper()));
+
+        }
+
+        public Word GetRandomWord()
+        {
+            Random r = new Random();
+            var number = r.Next(0,_words.Count);
+            return _words[number];
+        }
+    }
+}
